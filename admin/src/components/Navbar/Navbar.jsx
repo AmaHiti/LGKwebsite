e59ff1a4bdfdfd0c6974ad2f79de7a9eb301af9c
AmaHiti <amahitiara@gmail.com>
2025-05-07 23:18:24 +0530

    ading admin side changes

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,34 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 import { assets } from '../../assets/assets';
-import {useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const [token, setToken] = useState("");
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
+  
   useEffect(() => {
     const storedToken = localStorage.getItem("token");
     if (storedToken) {
       setToken(storedToken);
     }
   }, []);
-
+  
   const handleLogout = () => {
     localStorage.removeItem("token");
     setToken("");
-    navigate('/'); 
+    navigate('/');
   };
   
   return (
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
-      <div>
-        <div className="admin-section">
-          <p>Admin section</p>
-          {token && <button onClick={handleLogout}>Logout</button>}
-        </div>
+      
+      <div className="admin-section">
+        <p>Life's Good Kitchen</p>
       </div>
-      <img src={assets.profile_image} alt="" className="profile" />
+      
+      <div className="profile-section">
+        <img src={assets.profile_image} alt="" className="profile" />
+        {token && <button onClick={handleLogout}>Logout</button>}
+      </div>
     </div>
   );
 };
